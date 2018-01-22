62643a108cd1a2f87f4b1153a12af1384a09be8f
HaiqalAdam <33884196+HaiqalAdam@users.noreply.github.com>
2018-01-22 10:15:28 +0800

    Update Array-Exercise-2.cpp

--- Array-Exercise-2.cpp
+++ Array-Exercise-2.cpp
@@ -1,5 +1,6 @@
 //this is not a complete program. please finish the program. thank you.
-
+//name : MUHAMMAD HAIQAL ADAM BIN MOHD HIRWAN
+//id : A17DW0071
 
 #include <iostream>
 using namespace std;
@@ -9,16 +10,20 @@ int main()
 {
 	//insert different value to the third element
 	//tukar third element tu dengan value lain
-	mark[]=/*your valie*/
+	mark[2]=7;/*your valie*/
 
 	//get user input and insert sub the third element
 	//tukar the third element yang baru ditukar tu dengan input dari user
-	mark[x];
+	cin>>mark[2];
 
 	//print the first element of the array
-	mark[];
+	cout<<mark[0]<<endl;
 
 	//print the ith element of the array
-	mark[x]
+	cout<<mark[4]<<endl;
 
 }
+
+
+//updated
+//signed by his owner
