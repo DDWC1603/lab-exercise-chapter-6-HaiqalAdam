67e4cda9fc67e7e43435d4a5d2c1d60bbfb16138
HaiqalAdam <33884196+HaiqalAdam@users.noreply.github.com>
2018-01-22 10:32:14 +0800

    Update Array-Exercise-3.cpp

--- Array-Exercise-3.cpp
+++ Array-Exercise-3.cpp
@@ -1,6 +1,11 @@
 //dapatkan 10 input dan keluarkan hasil tambahnya.
 //hasilkan program anda dengan menggunakan array
-
+//name : MUHAMMAD HAIQAL ADAM BIN MOHD HIRWAN
+//id : A17DW0071
+#include <iostream>
+using namespace std;
+int main ()
+{
 
 int array[10];
 int sum;
@@ -9,13 +14,22 @@ int x;
 cout<<"Enter the 10 numbers:"<<endl;
 for(x=0;x<10;x++)
 {
+	cin>>array[x];
 
+	sum=sum+array[x];
 }
 
-cin>>array[x];
 
-sum=sum+array[x];
 
 cout<<"the sum of the 10 numbers : "<<sum<<endl;
 
 std::cin.get();
+}
+
+
+
+
+
+
+//updated 
+//hijau 
