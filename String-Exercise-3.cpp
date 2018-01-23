784ebc40ab8ffbd01bd612cda486b9ec790ee885
HaiqalAdam <33884196+HaiqalAdam@users.noreply.github.com>
2018-01-23 09:25:30 +0800

    Update String-Exercise-3.cpp

--- String-Exercise-3.cpp
+++ String-Exercise-3.cpp
@@ -1,24 +1,29 @@
 //use the string compare function to compare input string with the setup string
 //make a check with the string password(if else)
-
+//HAIQAL ADAM
+//A17DW0071
 #include <iostream>
-
+#include <string.h>
+using namespace std;
 int main()
 {
 	char strPswd[80];
-
+	
 	cout<<"Enter password: ";
-	//get the strPswd
-	
-	//make an if else that check the password(strPswd)
-	//(string compare function (strPswd,"Check")) 
-		
-		cout<<"invalid password "<<endl;
-	
+	gets(strPswd);
 	 
-		cout<<"Logged in "<<endl;
+	if(strcmp(strPswd,"password"))
+	{	
+		cout<<"invalid password "<<endl;
+	}
+	 
+	else cout<<"Logged in "<<endl;
 
 	
 
 	return 0;
 }
+
+
+//updated
+//signed by haiqal
